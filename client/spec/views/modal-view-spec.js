1e7e1f0a55d88db58d999d151796d421b4a93fbd
Ben Centra <bcentra@constantcontact.com>
2016-03-22 21:10:10 -0400

    adding modal-view-spec.js

---
+++ client/spec/views/modal-view-spec.js
@@ -0,0 +1,81 @@
+import ModalView from '../../src/views/modal-view';
+import Config from '../../src/config';
+import Backbone from 'backbone';
+
+describe('Modal View', () => {
+
+  let modalView = null;
+  let testData = null;
+  let testTemplate = null;
+  const mockConfig = new Config({
+    uid: 'bencentra',
+    cn: 'Ben Centra',
+    hostUrl: 'http://localhost:8888',
+    apiUrl: 'http://localhost:3000/v1'
+  });
+  const mockModel = new Backbone.Model({
+    config: mockConfig
+  });
+
+  beforeEach(() => {
+    testTemplate = '<div class="Modal">Test</div>';
+    testData = {test: 'lol'};
+    modalView = new ModalView({model: mockModel});
+    modalView.template = jasmine.createSpy('template').and.returnValue(testTemplate);
+  });
+
+  it('can be constructed', () => {
+    expect(modalView).toBeDefined();
+    expect(modalView.model).toEqual(mockModel);
+  });
+
+  describe('render', () => {
+
+    beforeEach(() => {
+      spyOn(modalView, 'delegateEvents');
+    });
+
+    it('renders the template', () => {
+      modalView.render(testData);
+      expect(modalView.template).toHaveBeenCalled();
+      expect(modalView.el.innerHTML).toEqual(testTemplate);
+      expect(modalView.delegateEvents).toHaveBeenCalled();
+    });
+
+    it('initializes the Bootstrap modal', () => {
+      modalView.render(testData);
+      expect(modalView.$modal).toBeDefined();
+      expect($.fn.modal).toHaveBeenCalledWith({show: false});
+    });
+
+    it('returns the view for chaining', () => {
+      const result = modalView.render(testData);
+      expect(result).toEqual(modalView);
+    });
+
+  });
+
+  describe('modal methods', () => {
+
+    beforeEach(() => {
+      modalView.render();
+    });
+
+    it('show() shows the modal', () => {
+      modalView.show();
+      expect($.fn.modal).toHaveBeenCalledWith('show');
+    });
+
+    it('hide() hides the modal', () => {
+      modalView.hide();
+      expect($.fn.modal).toHaveBeenCalledWith('hide');
+    });
+
+    it('toggle() toggles the modal', () => {
+      modalView.toggle();
+      expect($.fn.modal).toHaveBeenCalledWith('toggle');
+    });
+
+  });
+
+});
